3aaa194e0a3f00f96884321e9c5e9b0e973d438a
vudayani <udayani.vaka@gmail.com>
2025-09-24 22:06:22 +0530

    add mcp-client-demo, mcp-server-demo modules

---
+++ mcp-client-demo/src/test/java/com/example/mcpworkshop/client/McpClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcpworkshop.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mcp-server-demo/src/main/java/com/example/mcpworkshop/server/McpServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcpworkshop.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpServerDemoApplication.class, args);
+	}
+
+}
